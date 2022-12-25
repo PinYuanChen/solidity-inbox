81bf8a26fcac0fca134a9777ad0b1c7f8d58c560
Champion Chen <championchen@ChampiondeMacBook-Air.local>
2022-12-25 11:22:37 +0800

    [Test] refactor to async await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,15 +4,17 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // the constructor
 const web3 = new Web3(ganache.provider()) // the instance
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
     // Get a list of all accounts
-    web3.eth.getAccounts()
-        .then( fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
+    accounts = await web3.eth.getAccounts()
+        
     // Use one of those accounts to deploy a contract
 });
 
 describe('Inbox', () => {
-    it('deploy a contract', () => { });
+    it('deploy a contract', () => { 
+        console.log(accounts);
+    });
 });
