56cd4c04c7888b8a474ca79c0190275bee960822
Champion Chen <championchen@ChampiondeMacBook-Air.local>
2022-12-25 11:11:24 +0800

    [Test] fetch accounts from Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,28 +4,15 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // the constructor
 const web3 = new Web3(ganache.provider()) // the instance
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vrooom';
-    }
-}
-
-let car;
-
 beforeEach(() => {
-    car = new Car();
+    // Get a list of all accounts
+    web3.eth.getAccounts()
+        .then( fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+    // Use one of those accounts to deploy a contract
 });
 
-describe('Car', () => {
-   it('can park', () => {
-    assert.equal(car.park(), 'stopped');
-   }); 
-
-   it('can drive', () => {
-    assert.equal(car.drive(), 'vrooom');
-   });
+describe('Inbox', () => {
+    it('deploy a contract', () => { });
 });
